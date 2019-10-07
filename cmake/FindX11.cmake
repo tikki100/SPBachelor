5a081bd9e4873ff19cc3bb96bedd0fceb9168b4f
Oliver Magnus Madsen <oliver.m.m@hotmail.com>
2019-10-07 16:07:44 +0200

    Moved if(Apple) statement in cmake

--- cmake/FindX11.cmake
+++ cmake/FindX11.cmake
@@ -1,9 +1,11 @@
-find_package(X11 REQUIRED)
+if(APPLE)
+	find_package(X11 REQUIRED)
 
-message(STATUS "X11_FOUND = ${X11_FOUND}")
-message(STATUS "X11_INCLUDE_DIR = ${X11_INCLUDE_DIR}")
-message(STATUS "X11_LIBRARIES = ${X11_LIBRARIES}")
+	message(STATUS "X11_FOUND = ${X11_FOUND}")
+	message(STATUS "X11_INCLUDE_DIR = ${X11_INCLUDE_DIR}")
+	message(STATUS "X11_LIBRARIES = ${X11_LIBRARIES}")
 
-include_directories(${X11_INCLUDE_DIR})
+	include_directories(${X11_INCLUDE_DIR})
 
-link_directories(${X11_LIBRARIES})
+	link_directories(${X11_LIBRARIES})
+endif()
